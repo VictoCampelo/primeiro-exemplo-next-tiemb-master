0ae27ee978a018b857a3d3356f5fbac92f5537d2
Joao victor <victorcampelo040419@gmail.com>
2024-04-29 08:26:25 -0300

    first commit

---
+++ primeiro-exemplo-next-tiemb-master/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <link href ="/products"> Produtos</link>
+      
+      <p>Hello World... com Next.JS!</p>
+    </div>
+  );
+}
